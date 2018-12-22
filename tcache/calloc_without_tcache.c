0dcd9fd64d30aac113c26ea47f72dbffd0bf8f17
0xcc-Since2016 <0xccsince2016@gmail.com>
2018-12-22 07:24:42 +0000

    adding tcache calloc irrelevant example

---
+++ tcache/calloc_without_tcache.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//author : afang
+//comment: what a nice day, isn't it?!
+
+int main(){
+
+	char *temp1 = calloc(1,100);
+	char *temp2 = calloc(1,100);
+	free(temp1);
+	char *temp3 = calloc(1,50);
+	getchar();
+	return 0;
+}
+
